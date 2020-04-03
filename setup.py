85819ad00287fe8f2189ccd71c80ad8026ae7894
Shubham Khandalkar <shubham.sk473@gmail.com>
2020-04-03 15:49:28 +0530

    Setup.py updated for pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-client-tracker",
+    version="0.0.1",
+    author="Shubham Khandalkar",
+    author_email="shubham.sk473@gmail.com",
+    description="Client Tracker is a Django app to keep track of clients visiting your website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shubham-khandalkar/django-client-tracker/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    python_requires='>=3.6',
+)
